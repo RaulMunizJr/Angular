4dca6c4e08987b484e3eb4422feb1733c02fc14f
Raul Muniz <32320449+RaulMunizJr@users.noreply.github.com>
2019-10-01 14:13:43 -0500

    Revert "Added my project"
    This reverts commit 2f25be66d4e85f8736d711ac4781e2600826caec.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import {TSDemo1} from './Shared/Demo1'
+import { Component } from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  t1 : TSDemo1;
-  constructor(){
-    this.t1 = new TSDemo1();
-  }
-  ngOnInit(){
-    
-  }
+export class AppComponent {
+  title = 'angulardemoapp';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { DirectivesComponent } from './directives/directives.component';
-import { HighlightDirective } from './Shared/highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DirectivesComponent,
-    HighlightDirective
+    AppComponent
   ],
   imports: [
     BrowserModule
