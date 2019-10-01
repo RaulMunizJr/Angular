2f25be66d4e85f8736d711ac4781e2600826caec
Raul Muniz <32320449+RaulMunizJr@users.noreply.github.com>
2019-10-01 14:03:31 -0500

    Added my project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {TSDemo1} from './Shared/Demo1'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angulardemoapp';
+export class AppComponent implements OnInit {
+  t1 : TSDemo1;
+  constructor(){
+    this.t1 = new TSDemo1();
+  }
+  ngOnInit(){
+    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DirectivesComponent } from './directives/directives.component';
+import { HighlightDirective } from './Shared/highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DirectivesComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule
